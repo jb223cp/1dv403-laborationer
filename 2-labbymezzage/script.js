a4110556e3f93620c3debe7c6e823e9ced3eaf94
Jasmin Bejtovic <jb223cp@student.lnu.se>
2014-11-24 19:40:57 +0000

    Labb 2, moment 2

--- 2-labbymezzage/script.js
+++ 2-labbymezzage/script.js
@@ -1,6 +1,20 @@
 "use strict";
 
 window.onload = function(){
+    
+    var mess = new Message("test",new Date());
+    var now= new Date();
+    
+    alert(mess);
+    alert(mess.getText());
+    mess.setText("new test");
+    alert(mess);
+    alert(mess.getDate());
+    mess.setDate(now.getHours()+":"+now.getMinutes()+":"+now.getSeconds());
+    alert(mess.getDate());
+    
 
 	console.log("new");
 	};
+	
+	
