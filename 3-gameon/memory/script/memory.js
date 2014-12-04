e53f83e9c7b5dc40cea40158302863524914f654
Jasmin Bejtovic <jb223cp@student.lnu.se>
2014-12-04 01:27:04 +0000

    Finished, I have to add comments and clear the code

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -40,7 +40,15 @@ var Memory = {
                    imgTag.setAttribute("alt", "?");
                    link.setAttribute("href", "#");
                    link.setAttribute("class", "link");
+                   if(rows===cols){
                    placeHolder=(((rows*(i+1))-rows)+j);
+                   }
+                   else if(cols>rows){
+                       placeHolder=(rows*i)+i+j;
+                   }
+                   else{
+                       placeHolder=(cols*j)+i+j;
+                   }
                    
                    link.setAttribute("id", placeHolder);
                    
@@ -56,94 +64,96 @@ var Memory = {
         var previousPosition;
         var previousNumber;
         var counter=1;
+        var solved;
         var flag=true;
         
+        
         var el=document.body.getElementsByClassName('link');
-        for(var c=0;c<el.length;c++) {
+        for(var c=0 ;c<el.length; c++) {
             
+          
                el[c].onclick = function() {
                    
+                   if(flag===false){
+                       event.preventDefault(); 
+                   }
+  
                    unknown=this.id;
                    var isOdd = counter%2;
                    
                    if(unknown===previousPosition && isOdd===0){
                        event.preventDefault(); 
                    }
-                   
-                   
-                   
-                   
-                   
+
                    picNumber=Memory.squares[this.id];
                    
                    var current = document.getElementById(unknown);
                    var previous = document.getElementById(previousPosition);
-                   
-                   
-                       
-                       
-                       
+       
                        if (isOdd===1){
                            
                            current.firstChild.setAttribute("src", "pics/"+picNumber+".png");
                            /*current.setAttribute("onclick", "return false");*/
                            current.removeAttribute("href");
+                           current.setAttribute("class","linksolved");
                            
                        }
                        else{
-                           
-                           
-                           
-                           
+             
                                    current.firstChild.setAttribute("src", "pics/"+picNumber+".png");
-                                   /*current.setAttribute("onclick", "return false");*/
-                           
-                            
+                         
                                 if (previousNumber===picNumber && unknown!=previousPosition){
                                 
-                                   
                                    previous.firstChild.setAttribute("src", "pics/"+picNumber+".png");
                                    previous.setAttribute("onclick", "return false");
+                                   previous.setAttribute("class","linksolved");
+                                   current.setAttribute("class","linksolved");
                                    current.setAttribute("onclick", "return false");
                                    
                                 }
                                 else{
                                     
-                                    
-                                    setTimeout(function () {
-                               
-                                           current.firstChild.setAttribute("src", "pics/0.png");
-                                           previous.firstChild.setAttribute("src", "pics/0.png");
-                                           current.removeAttribute("onclick");
-                                           previous.removeAttribute("onclick");
-                                           previous.setAttribute("href", "#");
-                                           /*current.firstChild.setAttribute("onclick", "return false");*/
-                                           callback();
-                                       }, 1000);
-                                   
+                                   /*questionMark=document.body.getElementsByClassName('link');*/
+                                   flag=false;
+
+                     
+                                        setTimeout(function () {
+                                               flag=true;
+                                               current.firstChild.setAttribute("src", "pics/0.png");
+                                               previous.firstChild.setAttribute("src", "pics/0.png");
+                                               /*current.removeAttribute("onclick");
+                                               previous.removeAttribute("onclick");
+                                               previous.setAttribute("href", "#");
+                                               
+                                               previous.setAttribute("class","link");
+                                               current.setAttribute("class","link");*/
+                                        
+                                           }, 800);
+                                  
                                 }
-                           
-                           
                        }
                        
-                   
-                       
-                       
+                       // By setting current values on previous variables, secures that I can reach previous click values
                        previousNumber=picNumber;
                        previousPosition=unknown;
                        
+                       //Increase counter by one
                        counter++;
-                       var queue=2;
-                       queue=counter;
+                      
+                       solved=document.body.getElementsByClassName('linksolved');
                        
+                       //GAME OVER, writes how many tries were needed to finish the game (two clicks are one try)
+                       if(solved.length===(rows*cols)){
+                           
+                           var message = document.getElementById("counter");
+		                   message.innerHTML = "CONGRATULATION, You won after "+  Math.floor(counter/2) + " tries!!!";
+                           
+                       }
                        
                        return false;
-               
             }
         }
-        
     }
-    
 };
 
 window.onload = Memory.init;
